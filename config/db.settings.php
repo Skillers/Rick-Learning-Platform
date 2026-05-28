e645a5e3833b3e3bae67bc9bd6613eaba301b000
Rick Dörr <Rick.nl@hotmail.com>
2026-05-28 07:57:21 +0200

    Streak Card Created

--- config/db.settings.php
+++ config/db.settings.php
@@ -2,5 +2,5 @@
 define('DB_HOST',    'localhost');
 define('DB_NAME',    'Rick Learning Platform');
 define('DB_USER',    'root');
-define('DB_PASS',    '');
+define('DB_PASS',    '666NerullIsGod!');
 define('DB_CHARSET', 'utf8mb4');
